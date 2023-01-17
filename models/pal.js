c5a8e7387d94c799b2b6f041104821d565190b50
Yong Park <pyongho0306@gmail.com>
2023-01-17 11:22:35 -0800

    Clean up show view

--- models/pal.js
+++ models/pal.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+  content: String,
+  commenter: { type: Schema.Types.ObjectId, ref: "Profile" },
+});
+
 const palSchema = new Schema(
   {
     title: String,
