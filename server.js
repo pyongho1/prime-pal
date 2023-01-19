736a437ac7a15cf89754848ef360423468427ae2
Yong Park <pyongho0306@gmail.com>
2023-01-18 17:19:46 -0800

    Add profile route and view

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ import { router as indexRouter } from "./routes/index.js";
 import { router as authRouter } from "./routes/auth.js";
 import { router as palRouter } from "./routes/pals.js";
 import { router as availRouter } from "./routes/availabilities.js";
+import { router as profilesRouter } from "./routes/profiles.js";
 
 // create the express app
 const app = express();
@@ -68,7 +69,8 @@ app.use(passDataToView);
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
 app.use("/pals", palRouter);
-app.use('/availabilities', availRouter)
+app.use("/availabilities", availRouter);
+app.use("/profiles", profilesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
